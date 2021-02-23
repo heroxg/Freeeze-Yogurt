6b9042ea9ec5d69d3da99a3a845524bbb7ca9086
Henry Uyigue <don2much@gmail.com>
2021-02-23 06:51:41 -0800

    about page update

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Breadcrumb from './../../shared/breadcrumb/Breadcrumb'
 import breadcrumbImage from './../../assets/images/bg-header-about.jpg'
 
