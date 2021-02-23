405af1df2091f2f3c01ac759e30517a33ca05a0b
Henry Uyigue <don2much@gmail.com>
2021-02-23 07:02:50 -0800

    logo link

--- src/shared/nav/Nav.js
+++ src/shared/nav/Nav.js
@@ -14,7 +14,7 @@ class Nav extends Component {
             <Route>
                 <div id="header">
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <Link to="index.html" className="logo navbar-brand"><img src={Logo} alt="logo" /></Link>
+                        <Link to="/" className="logo navbar-brand"><img src={Logo} alt="logo" /></Link>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
